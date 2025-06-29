bb49e0e7dfb44b595c4aedd1683907583db2172d
OussamaAtifii <oussamaati03@gmail.com>
2025-06-29 14:14:51 +0200

    style: Set Geist as global font

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import App from './App.tsx'
+import '@fontsource-variable/geist'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
