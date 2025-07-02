5e550f93735f89b4627d066989721404efec57e5
OussamaAtifii <oussamaati03@gmail.com>
2025-07-02 19:39:03 +0200

    feat: Add tools route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router'
 import NavigationLayout from '@components/layouts/NavigationLayout.tsx'
 import Home from '@pages/Home.tsx'
+import ToolDetail from '@pages/ToolDetail'
 
 function App() {
   return (
     <Routes>
       <Route element={<NavigationLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/:category/:slug" element={<ToolDetail />} />
       </Route>
     </Routes>
   )
