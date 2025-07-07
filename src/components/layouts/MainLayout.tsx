b8301e0890923ddd5e8f625b05b95c85386b063a
Alexrsnchz <alexrsnchz@gmail.com>
2025-07-07 23:15:09 +0200

    refactor: Remove unused footer

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,22 +1,13 @@
 import Sidebar from '@components/navigation/sidebar/Sidebar.tsx'
 import { Outlet } from 'react-router'
-//import Footer from '@components/navigation/Footer.tsx'
 
 export default function MainLayout() {
   return (
-    <>
-      <div className="flex min-h-screen">
-        <Sidebar />
-        <main className="flex-1">
-          <Outlet />
-        </main>
-      </div>
-
-      {/*
-      <footer>
-        <Footer />
-      </footer>
-      */}
-    </>
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+    </div>
   )
 }
--- src/components/navigation/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="flex justify-between px-12 py-4 bg-neutral-300">
-      <span className="font-semibold text-gray-700">&copy; 2025 Toolsyde.</span>
-    </div>
-  )
-}
