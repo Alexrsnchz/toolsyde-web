8520682bef4af09b7eb18f439b1d8c294fa706c6
Alexrsnchz <alexrsnchz@gmail.com>
2025-07-11 13:40:35 +0200

    refactor: Delete unused getCategory function

--- src/assets/functions/navigation/GetCategory.ts
+++
@@ -1,5 +0,0 @@
-import type { LinkUtilProps } from '@customTypes/navigationTypes.ts'
-
-export function getCategory(data: LinkUtilProps[], text: string): LinkUtilProps | undefined {
-  return data.find(item => item.text === text)
-}
