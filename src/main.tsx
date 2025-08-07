540c99a0bc8d5d83e62d7125d2569a6a22a0e8b2
OussamaAtifii <oussamaati03@gmail.com>
2025-08-07 14:55:56 +0200

    chore: add geist mono font

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import App from './App.tsx'
 import '@fontsource-variable/geist'
+import '@fontsource-variable/geist-mono'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
